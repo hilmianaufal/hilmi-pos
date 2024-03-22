51f0ba2bd7f66283ffebf8cd8b4501fb0e718007
unknown <hilmiannaufal72@gmail.com>
2024-03-22 07:24:05 +0700

    Menambahkan Fitur Tambah Barang di Menu Barang

--- module/mode-beli.php
+++ module/mode-beli.php
@@ -18,5 +18,83 @@ function generateNo(){
     return $maxno;
 }
 
+function insert ($data){
+    global $koneksi;
+
+    $no   = mysqli_real_escape_string($koneksi, $data ['noBeli']);
+    $tgl   = mysqli_real_escape_string($koneksi, $data ['tglNota']);
+    $kode   = mysqli_real_escape_string($koneksi, $data ['kodeBrg']);
+    $nama   = mysqli_real_escape_string($koneksi, $data ['namaBrg']);
+    $qty   = mysqli_real_escape_string($koneksi, $data ['qty']);
+    $harga   = mysqli_real_escape_string($koneksi, $data ['harga']);
+    $jmlHarga   = mysqli_real_escape_string($koneksi, $data ['jmlHarga']);
+
+
+    $cekBrg = mysqli_query($koneksi, "SELECT * FROM tbl_beli_detail WHERE no_beli = '$no' AND kode_brg = '$kode'");
+    
+    if (mysqli_num_rows($cekBrg)){
+        echo "<script> 
+          alert ('Barang sudah ada , Anda Harus menghapusnya Dulu Jika menghapus Quantitynya');
+            
+            </script>";
+            return false;
+       }
+
+    if (empty($qty)){
+        echo "<script> 
+        alert ('Quantity Barang Tidak Boleh Kosong');
+    
+        </script>";
+        return false;
+    }else {
+        $sqlBeli = "INSERT INTO tbl_beli_detail VALUES ('','$no','$tgl','$kode','$nama',$qty,$harga,$jmlHarga)";
+        mysqli_query($koneksi, $sqlBeli);
+    }
+
+    mysqli_query($koneksi, "UPDATE tbl_barang SET stock = stock + $qty WHERE id_barang = '$kode' ");
+
+    return mysqli_affected_rows($koneksi);
+}
+
+function totalBeli ($nobeli){
+    global $koneksi;
+
+    $totalBeli = mysqli_query($koneksi, "SELECT sum(jumlah_harga) as total FROM tbl_beli_detail WHERE no_beli = '$nobeli' ");
+    $data = mysqli_fetch_assoc($totalBeli);
+
+    $total = $data ['total'];
+
+    return $total;
+}
+
+function deleteBrg($idBrg, $idBeli, $qty){
+    global $koneksi;
+
+    $sqlDel = "DELETE FROM tbl_beli_detail WHERE kode_brg = '$idBrg' AND no_beli = '$idBeli'";
+
+    mysqli_query($koneksi, $sqlDel);
+
+    mysqli_query($koneksi, "UPDATE tbl_barang SET stock = stock - $qty WHERE id_barang = '$idBrg' ");
+
+    return mysqli_affected_rows($koneksi);
+}
+
+function simpan ($data){
+    global $koneksi;
+
+    $nobeli = mysqli_real_escape_string($koneksi,$data['noBeli']);
+    $tgl = mysqli_real_escape_string($koneksi, $data['tglNota']);
+    $total = mysqli_real_escape_string($koneksi, $data['total']);
+    $supplier = mysqli_real_escape_string($koneksi, $data['supplier']);
+    $keterangan = mysqli_real_escape_string($koneksi, $data['ketr']);
+
+    $sqlBeli = "INSERT INTO tbl_beli_head VALUES ('$nobeli','$tgl','$supplier',$total,'$keterangan')";
+
+    mysqli_query($koneksi, $sqlBeli);
+
+    return mysqli_affected_rows($koneksi);
+
+}
+
 
 ?>
--- pembelian/index.php
+++ pembelian/index.php
@@ -18,6 +18,54 @@ require_once '../templates/header.php';
 require_once '../templates/navbar.php';
 require_once '../templates/sidebar.php';
 
+$kode = @$_GET['pilihbrg'] ? @$_GET['pilihbrg'] : '';
+
+if($kode){
+  $selectBrg = getDataBarang("SELECT* FROM tbl_barang WHERE id_barang = '$kode'")[0];
+}
+
+if (isset($_POST['addbrg'])) {
+  $tgl = $_POST['tglNota'];
+  if (insert($_POST)) {
+      echo "<script> 
+          alert('Data Berhasil Di Simpan');
+          document.location = '?tgl=$tgl';
+      
+      </script>";
+  }
+}
+
+if (isset($_POST['simpan'])) {
+ 
+  if (simpan($_POST)) {
+      echo "<script> 
+
+          alert('Data Berhasil Di Simpan');
+          document.location = 'index.php';
+      
+      </script>";
+  }
+}
+
+if (isset($_GET['msg'])){
+  $msg = $_GET ['msg'];
+}else {
+  $msg= '';
+}
+
+if($msg == 'deleted'){
+  $idBrg = $_GET['idbrg'];
+  $idBeli = $_GET['idbeli'];
+  $qty = $_GET['qty'];
+  $tgl = $_GET['tgl'];
+
+  deleteBrg($idBrg, $idBeli,$qty);
+  echo "<script> 
+  document.location = '?tgl=$tgl';
+
+  </script>";
+}
+
 $no_beli = generateNo();
 
 ?>
@@ -52,7 +100,7 @@ $no_beli = generateNo();
                                 </div>
                                 <label for="tglNota" class="col-sm-2 col-form-label">Tgl Nota</label>
                                 <div class="col-sm-4">
-                                    <input type="date" name="tglNota" id="tglNota" class="form-control" value="<?= date('Y-m-d') ?>" required>
+                                    <input type="date" name="tglNota" id="tglNota" class="form-control" value="<?= @$_GET['tgl'] ? $_GET['tgl'] :   date('Y-m-d') ?>" required>
                                 </div>
                             </div>
                             <div class="form-group row mb-2">
@@ -64,7 +112,7 @@ $no_beli = generateNo();
                                   <?php
                                       $barang = getDataBarang("SELECT * FROM tbl_barang");
                                       foreach($barang as $brg) { ?>
-                                            <option value="<?= $brg['id_barang'] ?>"><?= $brg['id_barang'] ?> | <?= $brg['nama_barang'] ?></option>
+                                            <option value="?pilihbrg=<?= $brg['id_barang'] ?><?= @$_GET['pilihbrg'] == $brg['id_barang'] ? 'selected' : '' ?>"><?= $brg['id_barang'] ?> | <?= $brg['nama_barang'] ?></option>
                                       <?php }
                                   ?>
     
@@ -76,7 +124,8 @@ $no_beli = generateNo();
                     <div class="col-md-6">
                       <div class="card card-outline card-danger pt-3 px-3 pb-2">
                                 <h6 class="font-weight-bold text-right">Total Pembelian</h6>
-                                <h1 class="font-weight-bold text-right" style="font-size: 40pt;">0</h1>
+                                <input type="hidden" name="total" value="<?= totalBeli($no_beli) ?>">
+                                <h1 class="font-weight-bold text-right" style="font-size: 40pt;"><?= number_format(totalBeli($no_beli),0,',','.') ?></h1>
                       </div>
                     </div>
                   </div>
@@ -84,38 +133,39 @@ $no_beli = generateNo();
                     <div class="row">
                       <div class="col-sm-4">
                         <div class="form-group ">
+                          <input type="hidden" value="<?= @$_GET['pilihbrg'] ? $selectBrg['id_barang'] : '' ?>" name="kodeBrg">
                           <label for="namaBrg"> Nama Barang</label>
-                          <input type="text" name="namaBrg" class="form-control form-control-sm" id="namaBrg" readonly>
+                          <input type="text" name="namaBrg" value="<?= @$_GET['pilihbrg'] ? $selectBrg['nama_barang'] : '' ?>" class="form-control form-control-sm" id="namaBrg" readonly>
                         </div>
                       </div>
                       <div class="col-sm-1">
                         <div class="form-group ">
                           <label for="stok">Stok</label>
-                          <input type="number" name="stok" class="form-control form-control-sm" id="stok" readonly>
+                          <input type="number" value="<?= @$_GET['pilihbrg'] ? $selectBrg['stock'] : '' ?>" name="stok" class="form-control form-control-sm" id="stok" readonly>
                         </div>
                       </div>
                       <div class="col-sm-1">
                         <div class="form-group ">
                           <label for="satuan">Satuan</label>
-                          <input type="text" name="satuan" class="form-control form-control-sm" id="satuan" readonly>
+                          <input type="text" name="satuan" value="<?= @$_GET['pilihbrg'] ? $selectBrg['satuan'] : '' ?>" class="form-control form-control-sm" id="satuan" readonly>
                         </div>
                       </div>
                       <div class="col-sm-2">
                         <div class="form-group ">
                           <label for="harga">Harga</label>
-                          <input type="number" name="harga" class="form-control form-control-sm" id="harga" readonly>
+                          <input type="number" value="<?= @$_GET['pilihbrg'] ? $selectBrg['harga_beli'] : '' ?>" name="harga" class="form-control form-control-sm" id="harga" readonly>
                         </div>
                       </div>
                       <div class="col-sm-2">
                         <div class="form-group ">
                           <label for="qty">Qty</label>
-                          <input type="number" name="qty" class="form-control form-control-sm" id="qty" >
+                          <input type="number" name="qty" value="<?= @$_GET['pilihbrg'] ? 1 : '' ?>" class="form-control form-control-sm" id="qty" >
                         </div>
                       </div>
                      <div class="col-sm-2">
                         <div class="form-group ">
                           <label for="jmlHarga">Harga</label>
-                          <input type="number" name="jmlHarga" class="form-control form-control-sm" id="jmlHarga" readonly>
+                          <input type="number" value="<?= @$_GET['pilihbrg'] ? $selectBrg['harga_beli'] : '' ?>" name="jmlHarga" class="form-control form-control-sm" id="jmlHarga" readonly>
                         </div>
                       </div>
                     </div>
@@ -127,15 +177,35 @@ $no_beli = generateNo();
                         <tr>
                           <th>No</th>
                           <th>Kode Barang</th>
-                          <th class="text-right">Nama Barang</th>
+                          <th >Nama Barang</th>
                           <th class="text-right">Harga</th>
-                          <th class="text-right">Qty</th>
-                          <th class="text-right">Jumlah Harga</th>
+                          <th class="text-center">Qty</th>
+                          <th >Jumlah Harga</th>
                           <th class="text-center" widht="10%">Action</th>
                         </tr>
                       </thead>
                       <tbody>
+                          <?php 
+                            $no = 1 ;
+                            $brgDetail = getDataBarang("SELECT * FROM tbl_beli_detail WHERE no_beli = '$no_beli'");
 
+                            foreach($brgDetail as $detail){ ?>
+                                <tr>
+                                  <td><?= $no++ ?></td>
+                                  <td><?= $detail['kode_brg'] ?></td>
+                                  <td><?= $detail['nama_barang'] ?></td>
+                                  <td class="text-right"><?= number_format( $detail['harga_beli'],0,',','.') ?></td>
+                                  <td class="text-center"><?= $detail['qty'] ?></td>
+                                  <td><?= $detail['jumlah_harga'] ?></td>
+                                  <td class="text-center">
+                                    <a href="?idbrg=<?= $detail['kode_brg'] ?>&idbeli=<?= $detail['no_beli'] ?>&qty=<?= $detail['qty'] ?>&tgl=<?= $detail['tgl_beli'] ?>&msg=deleted" class="btn btn-sm btn-danger" title="hpus brang" onclick="return confirm('Yakin ingin Menghapus Barang ini ?')"> <i class="fas fa-trash"></i></a>
+                                  </td>
+                                </tr>
+                            <?php 
+                            }
+                          
+                          ?>
+                        
                       </tbody>
                     </table>
                   </div>
@@ -144,7 +214,7 @@ $no_beli = generateNo();
                       <div class="form-group row mb-2">
                        <label for="" class="col-sm-3 col-form-label col-form-label-sm">Suplier</label>
                        <div class="col-sm-9">
-                         <select name="suplier" id="suplier" class="form-control form-control-sm">
+                         <select name="supplier" id="suplier" class="form-control form-control-sm">
                            <option value="">-- Pilih Suplier --</option>
                                  <?php
                                     $suplier = getDataSupplier("SELECT * FROM tbl_supplier");
@@ -170,5 +240,22 @@ $no_beli = generateNo();
             </form>
         </div>
     </section>
+
+
+    <script>
+      let pilihbrg = document.getElementById('kodeBrg')
+      pilihbrg.addEventListener('change', function(){
+        document.location.href = this.options[this.selectedIndex].value
+      })
+
+      let qty = document.getElementById('qty')
+      let jmlHarga = document.getElementById('jmlHarga');
+      let harga = document.getElementById('harga')
+      qty.addEventListener('input', function(){
+          jmlHarga.value = qty.value * harga.value;
+      })
+
+    </script>
+
 
 <?php require_once '../templates/footer.php'; ?>
