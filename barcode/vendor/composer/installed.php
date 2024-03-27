e955915f08b8b8fde65ea51512938a4a715fc69b
unknown <hilmiannaufal72@gmail.com>
2024-03-27 13:42:41 +0700

    Memperbaiki Eror Cetak Barcode Barang

--- barcode/vendor/composer/installed.php
+++ barcode/vendor/composer/installed.php
@@ -28,14 +28,5 @@
             'aliases' => array(),
             'dev_requirement' => false,
         ),
-        'setasign/fpdf' => array(
-            'pretty_version' => '1.8',
-            'version' => '1.8.0.0',
-            'reference' => 'a8a76e8cfc1db7b1a18c096a021a0cdff70db6b5',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../setasign/fpdf',
-            'aliases' => array(),
-            'dev_requirement' => false,
-        ),
     ),
 );
